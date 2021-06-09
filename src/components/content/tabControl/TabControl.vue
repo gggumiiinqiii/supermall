ace057cc398834164e8ebf02a8a570fe06f76d07
gggumiiinqiii <1378827345@qq.com>
2021-06-09 16:22:57 +0800

    tab组件

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tab-control">
+    <div class="tab-control-item" 
+    @click="change(index)"
+    v-for="(item,index) in title" :key="index"
+    :class="{active:index === currentindex}">
+      <span>{{title[index]}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabControl',
+  props:{
+    title:{
+      type:Array,
+      default(){
+        return[]
+      }
+    }
+  },
+  methods:{
+    change(index){
+      this.currentindex = index
+    }
+  },
+  data(){
+    return {
+      currentindex:0
+    }
+  }
+}
+</script>
+
+<style >
+.tab-control {
+  display: flex;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  font-size: 15px;
+  background-color: #fff;
+}
+.tab-control-item {
+  flex: 1;
+}
+.tab-control-item span {
+  padding: 5px;
+}
+.active {
+  color: var(--color-high-text);
+  
+}
+.active span {
+  border-bottom: 3px solid var(--color-tint);
+}
+</style>
